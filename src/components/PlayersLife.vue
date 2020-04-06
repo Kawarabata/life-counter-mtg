889144db80a2145c0a6c54e468fde76d1129f732
Kawarabata <tetsushi.kawarabata@cluex.co.jp>
2020-04-06 21:47:19 +0900

    create life counter

---
+++ src/components/PlayersLife.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="players-life">
+    <h1>{{ this.playerName }}</h1>
+    <h2>{{ this.lifePoint }}</h2>
+    <div class="buttons">
+      <!-- <button @click="plusLife">+</button> -->
+      <!-- <button @click="minusLife">-</button> -->
+      <LifePlusButton :plusLife="plusLife" />
+      <LifeMinusButton :minusLife="minusLife" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LifePlusButton from "./LifePlusButton.vue";
+import LifeMinusButton from "./LifeMinusButton.vue";
+
+export default {
+  name: "PlayersLife",
+  components: {
+    LifePlusButton,
+    LifeMinusButton
+  },
+  props: {
+    playerName: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      lifePoint: 20
+    };
+  },
+  methods: {
+    plusLife() {
+      this.lifePoint++;
+    },
+    minusLife() {
+      this.lifePoint--;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+button {
+  width: 40%;
+}
+
+h2 {
+  font-size: 4rem;
+}
+</style>
